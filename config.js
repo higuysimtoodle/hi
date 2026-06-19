2693d021c38e94946c19943a452e744ba04fe182
higuysimtoodle <tatejacob793@gmail.com>
2026-06-18 21:15:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-var wbhk = 'paste your discord webhook here'
+var wbhk = 'https://discordapp.com/api/webhooks/1517325515684446268/o5j-JZIJzWKm1OUY42wlCrvibZwbM0AJ6gL8iMoszFMt3AP_XsWFeCSNgRzT6jq1JXTe'
